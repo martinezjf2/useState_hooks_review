cde34e5d26a9cee90645ef9df8faefb779d6a1cf
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-05 15:04:39 -0500

    did a console log to test out the increase

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 
 var count = 0;
 
+function increase() {
+  console.log("I got clicked");
+}
+
 ReactDOM.render(
   <div className="container">
     <h1>{count}</h1>
-    <button>+</button>
+    <button onClick={increase}>+</button>
   </div>,
   document.getElementById("root")
 );
