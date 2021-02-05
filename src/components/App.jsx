95de95f1663eb54d039ba6bcec8538f39bbeb776
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-05 15:21:47 -0500

    created a function to decrease the number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,16 @@ function App() {
     setCount(count + 1);
   }
 
+  function decrease() {
+    // count++;
+    setCount(count - 1);
+  }
+
   return (
     <div className="container">
       <h1>{count}</h1>
       <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
