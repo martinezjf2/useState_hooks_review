050dc2031be9baf51ac8bcf59d63989ae18097b6
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-05 15:20:44 -0500

    used second in the array to be setCount and increment it by 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
 function App() {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   function increase() {
     // count++;
+    setCount(count + 1);
   }
 
   return (
